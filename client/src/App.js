9e74ca1408ba35a92c5660251165bece0058dd7c
Heath Ryu <32060873+heathryu@users.noreply.github.com>
2020-05-21 21:37:02 +0100

    Update dependency versions and refine the client side connection handling

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,38 @@
-import React, { useEffect, useRef, useState } from 'react';
-import socketIOClient from 'socket.io-client';
-import './App.css';
+import React, { useEffect, useRef, useState } from "react";
+import socketIOClient from "socket.io-client";
+import "./App.css";
 
-const App = props => {
+const App = () => {
   const [messages, setMessages] = useState([]);
   const socket = useRef();
 
   useEffect(() => {
-    socket.current = socketIOClient('localhost:3001');
+    socket.current = socketIOClient("localhost:3001");
   }, []);
 
   useEffect(() => {
-    socket.current.on('chat message', msg => {
+    socket.current.on("chat message", (msg) => {
       const newList = messages.concat(msg);
       setMessages(newList);
     });
-  }, [messages]);
+  });
 
-  const onSubmitHandler = event => {
+  useEffect(() => () => socket.current.close(), [socket]);
+
+  const onSubmitHandler = (event) => {
     event.preventDefault();
 
-    socket.current.emit('chat message', event.target.newMessage.value);
+    if (event.target.newMessage.value) {
+      socket.current.emit("chat message", event.target.newMessage.value);
 
-    event.target.newMessage.value = '';
+      event.target.newMessage.value = "";
+    }
   };
 
   return (
     <div>
       <ul id="messages">
-        {messages.map(m => (
+        {messages.map((m) => (
           <li>{m}</li>
         ))}
       </ul>
